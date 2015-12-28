af49a4c0b338014af3c4df4a9f5502fdd4a84027
Dylan Jew <dylanjew1@gmail.com>
2015-12-27 23:01:14 -0800

    game structure

--- src/io/jew/rain/Game.java
+++ src/io/jew/rain/Game.java
@@ -1,8 +1,51 @@
 package io.jew.rain;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
+	
+	private Thread thread;
+	private JFrame frame;
+	private boolean running = false;
+	
+	public Game() {
+		Dimension size = new Dimension(width*scale, height*scale);
+		setPreferredSize(size);
+		frame = new JFrame();
+	}
+	
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Display");
+		thread.start();
+	}
+	
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (running) {
+
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+		game.start();
+	}
+	
 }

4043f3df885af68802ed8e5a1462ebebdf16dd7c
kylejew <kylejew3@gmail.com>
2015-12-27 23:23:54 -0800

    finished game frame

--- src/io/jew/rain/Game.java
+++ src/io/jew/rain/Game.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 
 public class Game extends Canvas implements Runnable {
+	private static final long serialVersionUID = 1L;
 	
 	public static int width = 300;
 	public static int height = width / 16 * 9;
@@ -48,6 +49,12 @@ public class Game extends Canvas implements Runnable {
 		game.frame.setResizable(false);
 		game.frame.setTitle("Rain");
 		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		
+		game.start();
 	}
 	
 }

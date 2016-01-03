1dba128544f80ec8eab24be4f8e8f2475f507d6c
kylejew <kylejew3@gmail.com>
2016-01-02 16:47:58 -0800

    render screen

--- src/io/jew/rain/Game.java
+++ src/io/jew/rain/Game.java
@@ -10,6 +10,8 @@ import java.awt.image.DataBufferInt;
 
 import javax.swing.JFrame;
 
+import io.jew.rain.graphics.Screen;
+
 public class Game extends Canvas implements Runnable {
 	private static final long serialVersionUID = 1L;
 	
@@ -21,12 +23,16 @@ public class Game extends Canvas implements Runnable {
 	private JFrame frame;
 	
 	private boolean running = false;
+	private Screen screen;
 	
 	private BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 	private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
 	public Game() {
 		Dimension size = new Dimension(width * scale, height * scale);
 		setPreferredSize(size);
+		
+		screen = new Screen(width, height);
+		
 		frame = new JFrame();
 	}
 	
@@ -55,10 +61,15 @@ public class Game extends Canvas implements Runnable {
 			createBufferStrategy(3);
 			return;
 		}
+		screen.clear();
+		screen.render();
+		
+		for (int i = 0; i < pixels.length; i++) {
+			pixels[i] = screen.pixels[i];
+		}
 		
 		Graphics g = bs.getDrawGraphics();
-		g.setColor(Color.BLACK);
-		g.fillRect(0, 0, getWidth(), getHeight());
+		g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 		g.dispose();
 		bs.show();
 	}
---
+++ src/io/jew/rain/graphics/Screen.java
@@ -0,0 +1,28 @@
+package io.jew.rain.graphics;
+
+public class Screen {
+	private int width, height;
+	public int[] pixels;
+	
+	public Screen(int width, int height) {
+		this.width = width;
+		this.height = height;
+		pixels = new int[width * height];
+				
+	}
+	
+	public void clear() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				pixels[x + y * width] = 0x000000;
+			}
+		}
+	}
+	public void render() {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				pixels[x + y * width] = 0xff00ff;
+			}
+		}
+	}
+}

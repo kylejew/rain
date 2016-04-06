80c712f777dd6962218cca4a2daad093dc602cce
kylejew <kylejew3@gmail.com>
2016-04-06 13:20:05 -0700

    boundary

--- src/io/jew/rain/graphics/Screen.java
+++ src/io/jew/rain/graphics/Screen.java
@@ -4,6 +4,9 @@ public class Screen {
 	private int width, height;
 	public int[] pixels;
 	
+	int xtime = 50, ytime = 50;
+	int counter = 0;
+	
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
@@ -19,9 +22,15 @@ public class Screen {
 		}
 	}
 	public void render() {
+		counter++;
+		if (counter % 10 == 0) xtime--;
+		if (counter % 10 == 0) ytime--;
+		
 		for (int y = 0; y < height; y++) {
+			if (ytime < 0 || ytime >= height) break;
+			if (xtime < 0 || xtime >= width) break;
 			for (int x = 0; x < width; x++) {
-				pixels[x + y * width] = 0xff00ff;
+				pixels[xtime + ytime * width] = 0xff00ff;
 			}
 		}
 	}
